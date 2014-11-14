7e23ad7f71a5617a24e0befe5a12687bd538eb40
Hua Wang <cmaxvv@me.com>
2014-11-14 19:47:25 +0800

    add img loading by picasso

--- app/src/main/java/cn/bandu/oreader/fragments/ItemViewFragment.java
+++ app/src/main/java/cn/bandu/oreader/fragments/ItemViewFragment.java
@@ -1,9 +1,15 @@
 package cn.bandu.oreader.fragments;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 import cn.bandu.oreader.R;
 import cn.bandu.oreader.activity.MainActivity_;
@@ -15,13 +21,24 @@ import cn.bandu.oreader.activity.MainActivity_;
 @EFragment(R.layout.fragment_item_view)
 public class ItemViewFragment extends Fragment {
 
+    final static String TAG = ItemViewFragment.class.getSimpleName();
+
+    @ViewById
+    ImageView imgView;
+
+    @AfterViews
+    void afterViews() {
+        Log.e(TAG, "loading img");
+        Picasso.with(this.getActivity()).load("http://i.imgur.com/DvpvklR.png").into(imgView);
+    }
+
     public void onBackPressed() {
         MainActivity_ a = (MainActivity_) this.getActivity();
         a.hideItemView();
     }
 
     @Click
-    public void root(){
+    public void root() {
         MainActivity_ a = (MainActivity_) this.getActivity();
         a.hideItemView();
     }
