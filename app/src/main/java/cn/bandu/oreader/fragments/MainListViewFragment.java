04265e29261484b711a33d2bdceccb8370a49cc4
Hua Wang <cmaxvv@me.com>
2014-11-17 11:36:22 +0800

    pull to refresh

--- app/src/main/java/cn/bandu/oreader/fragments/MainListViewFragment.java
+++ app/src/main/java/cn/bandu/oreader/fragments/MainListViewFragment.java
@@ -1,19 +1,23 @@
 package cn.bandu.oreader.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.ViewById;
 
 import cn.bandu.oreader.R;
 import cn.bandu.oreader.activity.MainActivity_;
 import cn.bandu.oreader.adapter.ListAdapter;
 
 @EFragment(R.layout.fragment_list)
-public class MainListViewFragment extends ListFragment {
+public class MainListViewFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private final static String TAG = MainListViewFragment.class.getSimpleName();
 
@@ -21,11 +25,42 @@ public class MainListViewFragment extends ListFragment {
 
     private MainActivity_ mainActivity;
 
+    @ViewById
+    SwipeRefreshLayout listSwipeContainer;
+
+    @ViewById
+    ListView list;
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        Log.e(TAG, listSwipeContainer.toString());
+//        listSwipeContainer.setOnRefreshListener(this);
+//        listSwipeContainer.setColorScheme(android.R.color.holo_blue_bright,
+//                android.R.color.holo_green_light,
+//                android.R.color.holo_orange_light,
+//                android.R.color.holo_red_light);
+//    }
+
     @AfterViews
     public void afterViews() {
         mainActivity = (MainActivity_) getActivity();
         adapter = new ListAdapter(mainActivity);
-        setListAdapter(adapter);
+        list.setAdapter(adapter);
+        listSwipeContainer.setOnRefreshListener(this);
+        listSwipeContainer.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    public void onRefresh() {
+        Toast.makeText(mainActivity, "Refreshing", Toast.LENGTH_SHORT).show();
+//        new Handler().postDelayed(new Runnable() {
+//            @Override public void run() {
+//                listSwipeContainer.setRefreshing(false);
+//            }
+//        }, 5000);
     }
 
     @ItemClick
