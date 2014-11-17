9eae2a941c4fb8c85bf0b9a7b6b018f6ec63a1ea
Hua Wang <cmaxvv@me.com>
2014-11-17 09:53:30 +0800

    Add OreaderApplication_

--- app/src/main/java/cn/bandu/oreader/OReaderApplication.java
+++ app/src/main/java/cn/bandu/oreader/OReaderApplication.java
@@ -2,10 +2,13 @@ package cn.bandu.oreader;
 
 import android.app.Application;
 
+import org.androidannotations.annotations.EApplication;
+
 /**
  * Created by wanghua on 14/11/12.
  */
-public class MainApplication extends Application {
+@EApplication
+public class OReaderApplication extends Application {
 
 //    @Override
 //    public void onCreate(){
