530d1a69aa715dfc0bc22ef35782462d8938962a
杨明福 <ymf.watt@163.com>
2014-11-22 15:32:33 +0800

    左侧菜单

--- app/src/main/java/cn/bandu/oreader/fragments/SlidingMenuFragment.java
+++ app/src/main/java/cn/bandu/oreader/fragments/SlidingMenuFragment.java
@@ -1,32 +1,46 @@
 package cn.bandu.oreader.fragments;
 
 import android.support.v4.app.Fragment;
-import android.widget.ListView;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ViewById;
 
 import cn.bandu.oreader.R;
-import cn.bandu.oreader.adapter.SlidingMenuAdapter;
 
 /**
  * Created by yangmingfu on 14/11/13.
  */
-@EFragment(R.layout.listview_sliding_menu)
+@EFragment(R.layout.fragment_sliding_menu)
 public class SlidingMenuFragment extends Fragment{
-
-    @ViewById
-    ListView menuList;
-
 
     @AfterViews
     public void afterViews() {
-        menuList.setAdapter(new SlidingMenuAdapter(getActivity()));
+
     }
 
-//    @ItemClick
-//    public void itemClicked(int postion) {
-//
-//    }
+    @Click
+    public void favorite_btn() {
+
+    }
+
+    @Click
+    public void clear_cacahe_btn() {
+
+    }
+
+    @Click
+    public void about_btn() {
+
+    }
+
+    @Click
+    public void appstore_btn() {
+
+    }
+
+    @Click
+    public void feedback_btn() {
+
+    }
 }
