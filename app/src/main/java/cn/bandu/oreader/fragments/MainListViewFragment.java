1897d6dae399c9c650d758e31026bdbb04f94708
杨明福 <ymf.watt@163.com>
2014-11-22 14:10:10 +0800

    重写 SwipeRefreshLayout
    使SwipeRefreshLayout 支持上拉加载

--- app/src/main/java/cn/bandu/oreader/fragments/MainListViewFragment.java
+++ app/src/main/java/cn/bandu/oreader/fragments/MainListViewFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.widget.ListView;
 
 import org.androidannotations.annotations.AfterViews;
@@ -20,23 +19,22 @@ import java.util.Random;
 
 import cn.bandu.oreader.R;
 import cn.bandu.oreader.activity.DetailActivity_;
-import cn.bandu.oreader.activity.MainActivity_;
 import cn.bandu.oreader.adapter.ListAdapter;
 import cn.bandu.oreader.model.ListDo;
+import cn.bandu.oreader.view.SwipeRefreshLayout;
 
 @EFragment(R.layout.fragment_list)
-public class MainListViewFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class MainListViewFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, SwipeRefreshLayout.OnLoadListener {
 
     private final static String TAG = MainListViewFragment.class.getSimpleName();
 
     private ListAdapter adapter;
     private int i;
 
+    private boolean isRefreshing = false;
+
     List<ListDo> datas = new ArrayList<ListDo>();
 
-    private MainActivity_ mainActivity;
-
-    boolean freshing = false;
 
     @ViewById
     SwipeRefreshLayout listSwipeContainer;
@@ -47,25 +45,27 @@ public class MainListViewFragment extends Fragment implements SwipeRefreshLayout
     @AfterViews
     public void afterViews() {
         i = 0;
-        mainActivity = (MainActivity_) getActivity();
 
         genPullDatas();
 
-        adapter = new ListAdapter(mainActivity, datas);
+        adapter = new ListAdapter(getActivity(), datas);
         list.setAdapter(adapter);
 
         listSwipeContainer.setOnRefreshListener(this);
-        listSwipeContainer.setColorScheme(android.R.color.holo_blue_bright,
+        listSwipeContainer.setOnLoadListener(this);
+        listSwipeContainer.setColor(android.R.color.holo_blue_bright,
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
-    }
+        listSwipeContainer.setMode(SwipeRefreshLayout.Mode.BOTH);
+        listSwipeContainer.setLoadNoFull(false);
+     }
 
     private void genPullDatas() {
         Random random = new Random();
         List<ListDo> datasTmp = new ArrayList<ListDo>();
         //重构datas，将新数据放到上面
-        for (int j=0 ; j<20; j++) {
+        for (int j=0 ; j<2; j++) {
             ListDo listDo = new ListDo();
             listDo.title = "在流行歌曲还被认为是“靡靡之音”的时候 item - " + i;
             SimpleDateFormat sfd = new SimpleDateFormat("yyyy-MM-dd");
@@ -94,18 +94,58 @@ public class MainListViewFragment extends Fragment implements SwipeRefreshLayout
     }
 
     public void onRefresh() {
-        if (!freshing) {
-            freshing = true;
-            listSwipeContainer.setRefreshing(true);
+        if (!isRefreshing) {
+            isRefreshing = true;
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     genPullDatas();
                     adapter.notifyDataSetChanged();
                     listSwipeContainer.setRefreshing(false);
-                    freshing = false;
+                    isRefreshing = false;
                 }
-            }, 5000);
+            }, 500);
+        } else {
+            return;
+        }
+    }
+    private void genPushDatas() {
+        Random random = new Random();
+        //重构datas，将新数据放到上面
+        for (int j=0 ; j<2; j++) {
+            ListDo listDo = new ListDo();
+            listDo.title = "在流行歌曲还被认为是“靡靡之音”的时候 item - " + i;
+            SimpleDateFormat sfd = new SimpleDateFormat("yyyy-MM-dd");
+            listDo.createTime = sfd.format(new Date());
+            int tmp = random.nextInt(20) % 4;
+            listDo.description = "著名歌唱家王昆去世。上世纪80年代，在流行歌曲还被认为是“靡靡之音”的时候，她就曾力挺流行乐。歌很能打动人，所以我就批准他唱";
+
+
+            listDo.imageUrls = new ArrayList<String>();
+            listDo.model = tmp;
+            listDo.imageUrls.add("http://ww3.sinaimg.cn/thumbnail/7d47b003jw1emgmzkh5wsj20c3085ab3.jpg?" + random.nextInt(20));
+            listDo.imageUrls.add("http://ww2.sinaimg.cn/thumbnail/60718250jw1emida7kpogj20fa0ahwfc.jpg?" + random.nextInt(20));
+            listDo.imageUrls.add("http://ww2.sinaimg.cn/bmiddle/d0513221jw1emicwxta35j20dw0993zk.jpg?" + random.nextInt(20));
+
+            datas.add(listDo);
+            i++;
+        }
+
+    }
+    public void onLoad() {
+        if (!isRefreshing) {
+            isRefreshing = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    genPushDatas();
+                    adapter.notifyDataSetChanged();
+                    listSwipeContainer.setLoading(false);
+                    isRefreshing = false;
+                }
+            }, 500);
+        } else {
+            return;
         }
     }
 
